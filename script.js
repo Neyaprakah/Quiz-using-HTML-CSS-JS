13b6792ff01dd516a66aedb66ea64bdaf6726d91
Neyaprakash <141732433+Neyaprakah@users.noreply.github.com>
2024-07-22 16:48:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function a(){
+    document.getElementById("optionA").style="background-color: red; border: 1px solid red";
+    document.getElementById("optionA1").style="background-color: red; color: white; border: 1px solid red";
+}
+function b(){
+    document.getElementById("optionB").style="background-color: red; border: 1px solid red";
+    document.getElementById("optionB1").style="background-color: red; color: white; border: 1px solid red";
+}
+function c(){
+    document.getElementById("optionC").style="background-color: red; border: 1px solid red";
+    document.getElementById("optionC1").style="background-color: red; color: white; border: 1px solid red";
+}
+function d(){
+    document.getElementById("optionD").style="background-color: green; border: 1px solid green";
+    document.getElementById("optionD1").style="background-color: green; color: white; border: 1px solid green";
+}
